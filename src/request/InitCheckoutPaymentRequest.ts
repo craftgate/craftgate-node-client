351c9b3555cae8151ec6baeb79f8e32663e83472
Berkay Dinçer <berkay.dincer@craftbase.io>
2021-04-21 17:02:25 +0300

    adds allowOnlyCreditCard boolean flag to init checkout payment

--- src/request/InitCheckoutPaymentRequest.ts
+++ src/request/InitCheckoutPaymentRequest.ts
@@ -15,6 +15,7 @@ type InitCheckoutPaymentRequest = {
   cardUserKey?: string;
   callbackUrl: string;
   enabledInstallments: number[];
+  allowOnlyCreditCard?: boolean;
   items: PaymentItem[];
 };
 
