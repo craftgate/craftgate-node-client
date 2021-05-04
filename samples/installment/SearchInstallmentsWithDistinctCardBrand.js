cba46bfbcef976c254681a9024c6417cbb48d4c7
Alican Akkuş <alican.akkus94@gmail.com>
2021-05-04 10:00:16 +0300

    distinct card brand with lowest commissions while installment search (#30)

---
+++ samples/installment/SearchInstallmentsWithDistinctCardBrand.js
@@ -0,0 +1,18 @@
+const Craftgate = require('../../dist');
+
+const craftgate = new Craftgate.Client({
+  apiKey: 'api-key',
+  secretKey: 'secret-key',
+  baseUrl: 'https://sandbox-api.craftgate.io'
+});
+
+const request = {
+  binNumber: '525864',
+  price: 100,
+  currency: Craftgate.Model.Currency.TRY,
+  distinctCardBrandsWithLowestCommissions: true
+};
+
+craftgate.installment().searchInstallments(request)
+  .then(result => console.info('Search installments with distinct card brand', result))
+  .catch(err => console.error('Failed to search installments with distinct card brand', err));
--- src/request/SearchInstallmentsRequest.ts
+++ src/request/SearchInstallmentsRequest.ts
@@ -4,6 +4,7 @@ type SearchInstallmentsRequest = {
   binNumber?: string;
   price: number;
   currency?: Currency;
+  distinctCardBrandsWithLowestCommissions?: boolean;
 };
 
 export default SearchInstallmentsRequest;
