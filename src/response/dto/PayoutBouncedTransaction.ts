83771e663d53e1f7fe80b4934d8ec14cd5c7360e
Sabri Onur Tüzün <335067+sotuzun@users.noreply.github.com>
2021-06-18 11:48:36 +0300

    adds payout fields to bounced rows resource (#44)

--- src/response/dto/PayoutBouncedTransaction.ts
+++ src/response/dto/PayoutBouncedTransaction.ts
@@ -1,9 +1,14 @@
 type PayoutBouncedTransaction = {
   id: number;
   iban: string;
+  createdDate: Date;
+  updatedDate: Date;
+  payoutId: number;
+  payoutAmount: number;
   contactName: string;
   contactSurname: string;
   legalCompanyTitle: string;
+  rowDescription: string;
 };
 
 export default PayoutBouncedTransaction;
