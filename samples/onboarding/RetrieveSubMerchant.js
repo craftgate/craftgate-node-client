30c9cbd37478c9b753b1f9b3e8c61fe7145358dd
Lemi Orhan Ergin <lemi@craftbase.io>
2021-09-15 14:58:01 +0300

    adds comments

--- samples/onboarding/RetrieveSubMerchant.js
+++ samples/onboarding/RetrieveSubMerchant.js
@@ -6,6 +6,7 @@ const craftgate = new Craftgate.Client({
   baseUrl: 'https://sandbox-api.craftgate.io'
 });
 
+// change the memberId (displayed as 1 below) with a real id to run the sample
 craftgate.onboarding().retrieveMember(1)
   .then(result => console.info('Retrieved sub merchant member', result))
   .catch(err => console.error('Failed to retrieve sub merchant member', err));
--- samples/onboarding/SearchMembers.js
+++ samples/onboarding/SearchMembers.js
@@ -7,10 +7,10 @@ const craftgate = new Craftgate.Client({
 });
 
 const request = {
-  name: 'Zeytinyağı Üretim',
+  name: 'Zeytinyağı Üretim', // change name as you wish run the sample
   page: 0,
   size: 25,
-  memberIds: [1, 2],
+  memberIds: [1, 2], // change memberIds with a real ids to run the sample
   memberType: Craftgate.Model.MemberType.LimitedOrStockJointCompany
 };
 
