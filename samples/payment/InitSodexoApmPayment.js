965aa405788436d4bec247d9d60b0c6f8df35491
Alican Akkus <aakkus@ip-192-168-1-37.eu-central-1.compute.internal>
2022-06-13 12:25:59 +0300

    adds new alternative payment methods, sodexo and payoneer added

--- samples/payment/InitSodexoApmPayment.js
+++ samples/payment/InitSodexoApmPayment.js
@@ -3,7 +3,7 @@ const Craftgate = require('../../dist');
 const craftgate = new Craftgate.Client({
   apiKey: 'api-key',
   secretKey: 'secret-key',
-  baseUrl: 'http://localhost:8000'
+  baseUrl: 'https://sandbox-api.craftgate.io'
 });
 
 const request = {
