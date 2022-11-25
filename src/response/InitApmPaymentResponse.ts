2999016c1192025fac36b492b54db67bd20da51c
Alican Akkus <aakkus@ip-192-168-1-35.eu-central-1.compute.internal>
2022-11-25 12:23:43 +0300

    adds payment error detail to apm payment

--- src/response/InitApmPaymentResponse.ts
+++ src/response/InitApmPaymentResponse.ts
@@ -1,11 +1,14 @@
 import ApmAdditionalAction from '../model/ApmAdditionalAction';
 import PaymentStatus from '../model/PaymentStatus';
 
+import PaymentError from './dto/PaymentError';
+
 type InitApmPaymentResponse = {
   paymentId: number;
   redirectUrl: string;
   paymentStatus: PaymentStatus;
   additionalAction: ApmAdditionalAction;
+  paymentError: PaymentError;
 };
 
 export default InitApmPaymentResponse;
