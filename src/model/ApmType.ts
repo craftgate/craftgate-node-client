7002ae06deb561b8ef8ea2012a4879999f551e05
Burak BALDIRLIOGLU <burak.baldirlioglu@craftgate.io>
2022-12-20 18:02:16 +0300

    adds Klarna as an APM (#120)

--- src/model/ApmType.ts
+++ src/model/ApmType.ts
@@ -4,6 +4,7 @@ enum ApmType {
   Sodexo = 'SODEXO',
   Edenred = 'EDENRED',
   PayPal = 'PAYPAL',
+  Klarna = 'KLARNA',
   Afterpay = 'AFTERPAY',
   FundTransfer = 'FUND_TRANSFER',
   CashOnDelivery = 'CASH_ON_DELIVERY'
