bdb53b4c94e856f136e0a45394140088fc4be2e4
Alican Akkuş <alican.akkus@craftgate.io>
2023-02-06 11:07:23 +0300

    adds bank commission and merchant commission rates to installment response (#122)

--- src/response/dto/InstallmentPrice.ts
+++ src/response/dto/InstallmentPrice.ts
@@ -2,6 +2,8 @@ type InstallmentPrice = {
   posAlias: string;
   installmentNumber: number;
   installmentPrice: number;
+  bankCommissionRate: number;
+  merchantCommissionRate: number;
   totalPrice: number;
   installmentLabel: string;
   loyaltySupported: boolean;
