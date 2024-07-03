b4010b617c778039e387ae1f69ad535025a26ea8
Derya Cakmak <97deryacakmak@gmail.com>
2024-07-03 11:40:11 +0300

    update

--- src/model/PaymentAuthenticationType.ts
+++ src/model/PaymentAuthenticationType.ts
@@ -1,7 +1,7 @@
 enum PaymentAuthenticationType {
   ThreeDs = 'THREE_DS',
   NonThreeDs = 'NON_THREE_DS',
-  BKM_EXPRESS = 'BKM_EXPRESS'
+  BkmExpress = 'BKM_EXPRESS'
 }
 
 export default PaymentAuthenticationType;
