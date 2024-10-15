6e4983f574b5ed30ef1064013dbaa9b9b063a6d0
deryacakmak <97deryacakmak@gmail.com>
2024-10-15 11:46:37 +0300

    update

--- src/model/WebhookEventType.ts
+++ src/model/WebhookEventType.ts
@@ -10,8 +10,8 @@ enum WebhookEventType {
   WalletCreated = 'WALLET_CREATED',
   WalletTxCreated = 'WALLET_TX_CREATED',
   BnplNotification = 'BNPL_NOTIFICATION',
-  MULTI_PAYMENT_COMPLETED = 'MULTI_PAYMENT_COMPLETED',
-  BKM_EXPRESS_PAYMENT_NOTIFICATION = 'BKM_EXPRESS_PAYMENT_NOTIFICATION'
+  MultiPaymentCompleted = 'MULTI_PAYMENT_COMPLETED',
+  BkmExpressPaymentNotification = 'BKM_EXPRESS_PAYMENT_NOTIFICATION'
 }
 
 export default WebhookEventType;
