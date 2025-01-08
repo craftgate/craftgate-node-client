a3cfbb499d631dd17ed3137e5addcf0f6d799938
Ertuğrul Berat ALLAHVERDİ <berat.allahverdi@hotmail.com>
2025-01-08 11:18:29 +0300

    added iwallet apm

---
+++ samples/payment/CompleteIWalletApmPayment.js
@@ -0,0 +1,16 @@
+const Craftgate = require('../../dist');
+
+const craftgate = new Craftgate.Client({
+  apiKey: 'api-key',
+  secretKey: 'secret-key',
+  baseUrl: 'https://sandbox-api.craftgate.io'
+});
+
+const request = {
+  paymentId: 1,
+  additionalParams: {"otpCode": "1234"}
+};
+
+craftgate.payment().completeApmPayment(request)
+    .then(result => console.info('Edenred Apm complete payment successful', result))
+    .catch(err => console.error('Failed to complete Edenred Apm payment', err));
