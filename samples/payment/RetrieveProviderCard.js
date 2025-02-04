40c0a6279bcbbbdcf5a2a4af8ab4f3b18011eb17
Semih Şahan <s.semih.sahan@hotmail.com>
2025-02-04 10:21:35 +0300

    Change response type of retrieve provider card (#230)
    * Change response type of retrieve provider card
    
    * Update

--- samples/payment/RetrieveProviderCard.js
+++ samples/payment/RetrieveProviderCard.js
@@ -12,6 +12,6 @@ const request = {
   providerCardUserId: "0309ac2d-c5a5-4b4f-a91f-5c444ba07b24"
 };
 
-craftgate.payment().retrieveProviderCard(request)
+craftgate.payment().retrieveProviderCards(request)
   .then(result => console.info('Retrieve migrated provider card', result))
   .catch(err => console.error('Failed to retrieve migrated provider card', err));
