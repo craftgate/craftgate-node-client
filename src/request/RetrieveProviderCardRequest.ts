a9bcec15681bb2e508172b2699b8039d034de9e7
Semih Şahan <semih.sahan.info@gmail.com>
2025-02-06 15:42:28 +0300

    Add CardMigrationProvider enum for RetrieveProviderCardRequest

--- src/request/RetrieveProviderCardRequest.ts
+++ src/request/RetrieveProviderCardRequest.ts
@@ -1,10 +1,10 @@
-import CardMigrationProvider from "../model/CardMigrationProvider";
+import CardMigrationProvider from '../model/CardMigrationProvider';
 
 type RetrieveProviderCardRequest = {
-    providerCardToken: string;
-    externalId: string;
-    providerCardUserId: string;
-    cardMigrationProvider: CardMigrationProvider;
+  providerCardToken: string;
+  externalId: string;
+  providerCardUserId: string;
+  cardMigrationProvider: CardMigrationProvider;
 };
 
 export default RetrieveProviderCardRequest;
