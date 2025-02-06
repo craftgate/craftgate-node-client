ac42ea6dc3cd97c96dde415725920d9afeef7048
Serhat Tunca <serhat.tunca@craftgate.io>
2025-02-06 15:24:35 +0300

    Add settlement earnings destination "NONE"

--- src/model/SettlementEarningsDestination.ts
+++ src/model/SettlementEarningsDestination.ts
@@ -1,7 +1,8 @@
 enum SettlementEarningsDestination {
   Iban = 'IBAN',
   Wallet = 'WALLET',
-  CrossBorder = 'CROSS_BORDER'
+  CrossBorder = 'CROSS_BORDER',
+  Node = 'NONE'
 }
 
 export default SettlementEarningsDestination;
