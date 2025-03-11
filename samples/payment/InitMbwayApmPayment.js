094a85722ff169cabcdb1f7703456233bdd36b77
Avni BALIKCI <108258717+abalikci@users.noreply.github.com>
2025-03-11 16:21:57 +0300

    Add Paylands - MB WAY (#244)

---
+++ samples/payment/InitMbwayApmPayment.js
@@ -0,0 +1,33 @@
+const Craftgate = require('../../dist');
+
+const craftgate = new Craftgate.Client({
+    apiKey: 'api-key',
+    secretKey: 'secret-key',
+    baseUrl: 'https://sandbox-api.craftgate.io'
+});
+
+const request = {
+    apmType: Craftgate.Model.ApmType.PaylandsMbWay,
+    price: 1.0,
+    paidPrice: 1.0,
+    currency: Craftgate.Model.Currency.EUR,
+    paymentGroup: Craftgate.Model.PaymentGroup.ListingOrSubscription,
+    conversationId: 'conversationId',
+    items: [
+        {
+            name: 'Item 1',
+            price: 0.4,
+            externalId: '123d1297-839e-4bd6-a13b-4be31a6e12a8'
+        },
+        {
+            name: 'Item 2',
+            price: 0.6,
+            externalId: '789d1297-839e-4bd6-a13b-4be31a6e13f7'
+        }
+    ],
+    additionalParams: {"buyerPhoneNumber": "34700000000"}
+};
+
+craftgate.payment().initApmPayment(request)
+    .then(result => console.info('Paylands - MB WAY Apm init payment successful', result))
+    .catch(err => console.error('Failed to init Paylands - MB WAY Apm payment', err));
