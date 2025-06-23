20eca76c2f41af5a8f7e5f512a935d29e15a8663
Derya Çakmak <36774966+deryacakmak@users.noreply.github.com>
2025-06-23 14:35:04 +0300

    Add cardBrandInstallments parameter (#257)

--- src/model/PaymentSource.ts
+++ src/model/PaymentSource.ts
@@ -1,7 +1,11 @@
 enum PaymentSource {
   API = 'API',
   CheckoutForm = 'CHECKOUT_FORM',
-  PayByLink = 'PAY_BY_LINK'
+  PayByLink = 'PAY_BY_LINK',
+  BkmExpress = 'BKM_EXPRESS',
+  Hepsipay = 'HEPSIPAY',
+  Juzdan = 'JUZDAN',
+  Mono = 'MONO'
 }
 
 export default PaymentSource;
