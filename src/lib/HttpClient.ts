176187be933e7024f71efc6482fe5fe96161194c
Emre INAL <emre.inal@craftgate.io>
2025-09-24 17:03:39 +0300

    prepares for next release v1.0.62

--- src/lib/HttpClient.ts
+++ src/lib/HttpClient.ts
@@ -85,7 +85,7 @@ export class HttpClient {
     config.headers[API_KEY_HEADER_NAME] = this._options.apiKey;
     config.headers[RANDOM_HEADER_NAME] = randomStr;
     config.headers[AUTH_VERSION_HEADER_NAME] = '1';
-    config.headers[CLIENT_VERSION_HEADER_NAME] = 'craftgate-node-client:1.0.61';
+    config.headers[CLIENT_VERSION_HEADER_NAME] = 'craftgate-node-client:1.0.62';
     if (this._options.language) {
       config.headers[LANGUAGE_HEADER_NAME] = this._options.language;
     }
