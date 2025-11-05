b195af0ee35159ec885309b40cd494dca94a7df3
deryacakmak <97deryacakmak@gmail.com>
2025-11-05 09:55:08 +0300

    Update fraud process for loyalty

--- samples/fraud/AddValueToFraudValueList.js
+++ samples/fraud/AddValueToFraudValueList.js
@@ -4,7 +4,7 @@ const Craftgate = require('../../dist');
 const craftgate = new Craftgate.Client({
   apiKey: 'api-key',
   secretKey: 'secret-key',
-  baseUrl: 'http://localhost:8000'
+  baseUrl: 'https://sandbox-api.craftgate.io'
 });
 
 //request
