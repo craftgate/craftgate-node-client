e1e40a2b84c973253a0842b6e680aaa04b92e8b7
Semih Şahan <s.semih.sahan@hotmail.com>
2025-11-07 10:22:37 +0300

    Add new feature for downloading report (#285)
    * Add sodexo gift as a apm
    
    * Add new feature for downloading report
    
    * Update
    
    * Update
    
    * Update
    
    * Update

--- src/adapter/FileReportingAdapter.ts
+++ src/adapter/FileReportingAdapter.ts
@@ -1,9 +1,12 @@
+import CreateReportRequest from '../request/CreateReportRequest';
 import {ClientCreationOptions} from '../lib/HttpClient';
 
 import RetrieveDailyPaymentReportRequest from '../request/RetrieveDailyPaymentReportRequest';
 import RetrieveDailyTransactionReportRequest from '../request/RetrieveDailyTransactionReportRequest';
 
 import BaseAdapter from './BaseAdapter';
+import ReportDemandResponse from '../response/ReportDemandResponse';
+import RetrieveReportRequest from '../request/RetrieveReportRequest';
 
 export default class FileReportingAdapter extends BaseAdapter {
   constructor(options: ClientCreationOptions) {
@@ -16,5 +19,13 @@ export default class FileReportingAdapter extends BaseAdapter {
 
   async retrieveDailyPaymentReport(request: RetrieveDailyPaymentReportRequest): Promise<ArrayBuffer> {
     return this._client.get<ArrayBuffer>('/file-reporting/v1/payment-reports', request, {responseType: 'arraybuffer'});
+  }
+
+  async createReport(request: CreateReportRequest): Promise<ReportDemandResponse> {
+    return this._client.post('/file-reporting/v1/report-demands', request);
+  }
+
+  async retrieveReport(request: RetrieveReportRequest, reportId: number): Promise<ArrayBuffer> {
+    return this._client.get<ArrayBuffer>('/file-reporting/v1/reports/' + reportId, request, {responseType: 'arraybuffer'});
   }
 }
