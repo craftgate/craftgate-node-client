cb6f3590bdb42b721ff897d71159a7c66c7417ba
deryacakmak <97deryacakmak@gmail.com>
2025-12-16 11:26:58 +0300

    Add `BkmExpress` to PaymentProvider and remove it from PaymentSource

--- src/model/PaymentSource.ts
+++ src/model/PaymentSource.ts
@@ -2,7 +2,6 @@ enum PaymentSource {
   API = 'API',
   CheckoutForm = 'CHECKOUT_FORM',
   PayByLink = 'PAY_BY_LINK',
-  BkmExpress = 'BKM_EXPRESS',
   Hepsipay = 'HEPSIPAY',
   Juzdan = 'JUZDAN',
   Mono = 'MONO'
