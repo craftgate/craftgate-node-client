6ede4f11ae5140816e293882a0e64779ef8ad6b4
Serhat Tunca <serhat.tunca@craftgate.io>
2026-07-13 15:23:55 +0300

    Remove `paidPrice` field from `InitMultiPaymentRequest`.

--- samples/payment/InitMultiPayment.js
+++ samples/payment/InitMultiPayment.js
@@ -8,7 +8,6 @@ const craftgate = new Craftgate.Client({
 
 const request = {
     price: 100.0,
-    paidPrice: 100.0,
     callbackUrl: 'https://www.your-website.com/craftgate-checkout-callback',
     currency: Craftgate.Model.Currency.TRY,
     conversationId: '456d1297-908e-4bd6-a13b-4be31a6e47d5',
